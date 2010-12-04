e92a24a15814ec110048336fb6fd68a12ba000e6
Thomas Murach <asiasuppenesser@gmx.de>
2010-12-04 19:07:45 +0100

    prozesse richtig rum. 1000 wdholungen

--- 05/code/02/pingpong.c
+++ 05/code/02/pingpong.c
@@ -24,46 +24,49 @@ int main(int argc, char *argv[])
 }
 
 
+void proc1()
+{
+  int rank, msg, task;
+    MPI_Status status;
+    int i, wiederholungen;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     printf("rank proc1 %d\n", rank);
+
+    for (i = 0; i < arrlength; i++){
+      for (wiederholungen = 0; wiederholungen < 1000; wiederholungen++){
+	MPI_Send(&msg, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	MPI_Recv(&task, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+
+	printf("rank %d: working on task %d\n", rank, task);
+// 	system("sleep 0.001");
+      }
+    }
+//     printf("test2\n");
+}
+
+
 void proc0()
 {
-    int size, msg, task, dest;
+    int size, msg, task, dest, rank;
+    int wiederholungen;
     MPI_Status status;
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//     printf("rank proc0 %d\n", rank);
     
     for (task = 1; task <= arrlength; task++) {	
-//       task = 1;
-      MPI_Recv(&msg, 0, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-      MPI_Send(&task, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+      for (wiederholungen = 0; wiederholungen < 1000; wiederholungen++){
+	MPI_Recv(&msg, 0, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+	MPI_Send(&task, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      }
     }
-    printf("test0\n");
+//     printf("test0\n");
 
-    task = -1;
+//     task = -1;
 //     for (dest = 1; dest < size; dest++) {
 // 	MPI_Recv(&msg, 0, MPI_INT, dest, 0, MPI_COMM_WORLD, &status);
 // 	MPI_Send(&task, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
 //     }
-    printf("test1\n");
-}
-
-
-void proc1()
-{
-    int rank, msg, task;
-    MPI_Status status;
-    int i;
-
-    for (i = 0; i < arrlength; i++){
-		
-// 		while(1) {
-			MPI_Send(&msg, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
-			MPI_Recv(&task, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-		
-// 			if (task < 0) break;
-		
-			printf("rank %d: working on task %d\n", rank, task);
-			system("sleep 1");
-// 		}
-    }
-    printf("test2\n");
+//     printf("test1\n");
 }
