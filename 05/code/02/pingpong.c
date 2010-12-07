3238ff4096028854998e0bfc943cbb08e53280f6
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-12-07 20:41:10 +0100

    mit zeitmessung

--- 05/code/02/pingpong.c
+++ 05/code/02/pingpong.c
@@ -2,24 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void master();
-void slave();
-const int length[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
-const int arrlength = sizeof(length)/sizeof(length[0]);
-char *msg;
-int rank, r, i;
-MPI_Status status;
+double wall_time(void);
 
 int main(int argc, char *argv[])
 {
+  const int length[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+  const int arrlength = sizeof(length)/sizeof(length[0]);
+  const int r_max = 1000; // wiederholungen
+  char *msg;
+  int rank, r, i;
+  MPI_Status status;
+  double time, ttime;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   for (i = 0; i < arrlength; i++){
     msg = (char *)malloc(length[i]);
-    for (r = 0; r < 1000; r++){
-      printf("rank %d, %d, %d\n", rank, i, r);
+
+    if(rank == 0) {
+      time = wall_time();
+      time = wall_time();
+    }
+
+    for (r = 0; r < r_max; r++){
+      // printf("rank %d, %d, %d\n", rank, i, r);
       if(rank == 0) {
         MPI_Send(msg, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD);
         MPI_Recv(msg, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
@@ -28,6 +35,12 @@ int main(int argc, char *argv[])
         MPI_Send(msg, length[i], MPI_CHAR, 0, 0, MPI_COMM_WORLD);
       }
     }
+
+    if(rank == 0) {
+      time = wall_time() - time;
+      printf("Zeit um %7d Bytes 2*%d mal zu übertragen: %g s\n",length[i], r_max, time / r_max);
+    }
+
     free(msg);
   }
 
