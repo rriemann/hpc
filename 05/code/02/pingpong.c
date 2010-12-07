eb799466ac81f20b3adf3a4bf81ca1df6415e357
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-12-07 19:57:51 +0100

    bug fixed :)

--- 05/code/02/pingpong.c
+++ 05/code/02/pingpong.c
@@ -6,8 +6,8 @@ void master();
 void slave();
 const int length[] = {1, 10, 100, 1000, 10000, 100000, 1000000};
 const int arrlength = sizeof(length)/sizeof(length[0]);
-char *msg, *msg2;
-int rank;
+char *msg;
+int rank, r, i;
 MPI_Status status;
 
 int main(int argc, char *argv[])
@@ -16,58 +16,20 @@ int main(int argc, char *argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  if(rank == 0) {
-    master();
-  } else {
-    slave();
+  for (i = 0; i < arrlength; i++){
+    msg = (char *)malloc(length[i]);
+    for (r = 0; r < 1000; r++){
+      printf("rank %d, %d, %d\n", rank, i, r);
+      if(rank == 0) {
+        MPI_Send(msg, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(msg, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
+      } else {
+        MPI_Recv(msg, length[i], MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Send(msg, length[i], MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+      }
+    }
+    free(msg);
   }
 
   MPI_Finalize();
-}
-
-
-void master()
-{
-  int task;
-  int i, wiederholungen;
-
-  for (i = 0; i < arrlength; i++){
-//     printf("vor malloc\n");
-    msg = (char *)malloc(length[i]);
-    msg2 = (char *)malloc(length[i]*20);
-//     printf("nach malloc\n");
-    for (wiederholungen = 0; wiederholungen < 1000; wiederholungen++){
-      printf("rank %d, %d, %d\n", rank, i, wiederholungen);
-//       memset(msg, 0x0,length[i]);
-      MPI_Send(msg, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-//       printf("vor rec\n");
-      MPI_Recv(msg2, length[i], MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
-//       printf("nach rec\n");
-    }
-//     printf("vor free\n");
-    free(msg);
-    free(msg2);
-  }
-}
-
-
-void slave()
-{
-  int dest, i, wiederholungen;
-  for(i = 0; i < arrlength; i++) {
-
-    msg = (char *)malloc(length[i]);
-    msg2 = (char *)malloc(length[i]*20);
-    for (wiederholungen = 0; wiederholungen < 1000; wiederholungen++){
-      printf("rank %d, %d, %d\n", rank, i, wiederholungen);
-//       printf("vor rec\n");
-      MPI_Recv(msg2, length[i], MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-//       printf("nach rec\n");
-//       memset(msg, 0x0,length[i]);
-      MPI_Send(msg, length[i], MPI_INT, 0, 0, MPI_COMM_WORLD);
-//       printf("nach send\n");
-    }
-    free(msg);
-    free(msg2);
-  }
 }
