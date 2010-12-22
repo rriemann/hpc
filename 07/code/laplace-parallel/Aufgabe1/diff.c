5558200a8a7881bb13b4c6c94afdfa5f78546793
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-12-22 13:54:41 +0100

    better usage of allreduce

--- 07/code/laplace-parallel/Aufgabe1/diff.c
+++ 07/code/laplace-parallel/Aufgabe1/diff.c
@@ -1,5 +1,15 @@
 # include <math.h>
 # include "laplace.h"
+#include "decomp.h"
+#include "mpi.h"
+
+double global_sum(double localsum)
+{
+  double sum = 0;
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Allreduce( &localsum, &sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD );
+  return sum;
+}
 
 double diff(field v1, field v2, int Nx, int Ny)
 {
@@ -12,6 +22,8 @@ double diff(field v1, field v2, int Nx, int Ny)
             d = v1[y][x] - v2[y][x];
             sum += d * d;
         }
+
+    sum = global_sum(sum);
 
     return sqrt(sum);
 }
