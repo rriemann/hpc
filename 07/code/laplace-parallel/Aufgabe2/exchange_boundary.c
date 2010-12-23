078351574696f4ea04bad6b300ba6672fc5b9198
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-12-23 20:28:11 +0100

    7.2 geht

--- 07/code/laplace-parallel/Aufgabe2/exchange_boundary.c
+++ 07/code/laplace-parallel/Aufgabe2/exchange_boundary.c
@@ -8,36 +8,23 @@ void exchange_boundary(field v, int Lx, int Ly)
 {
   int y;
   MPI_Status status;
-  
+
   MPI_Datatype Row;
   MPI_Type_contiguous(Lx, MPI_DOUBLE, &Row);
   MPI_Type_commit(&Row);
 
-//   MPI_Ssend(&v[Ly][1],  1, Row, decomp.north, 0, MPI_COMM_WORLD);
-//   MPI_Recv(&v[Ly+1][1], 1, Row, decomp.north, 0, MPI_COMM_WORLD, &status);
   MPI_Sendrecv(&v[Ly][1], 1, Row, decomp.north, 0, &v[Ly+1][1], 1, Row, decomp.north, 0, MPI_COMM_WORLD, &status);
-  
-//   MPI_Ssend(&v[1][1],   1, Row, decomp.south, 0, MPI_COMM_WORLD);
-//   MPI_Recv(&v[0][1],    1, Row, decomp.south, 0, MPI_COMM_WORLD, &status);
+
   MPI_Sendrecv(&v[1][1], 1, Row, decomp.south, 0, &v[0][1], 1, Row, decomp.south, 0, MPI_COMM_WORLD, &status);
 
   MPI_Type_free(&Row);
-  
-//   for (y = 1; y <= Ly; y++) {
-//      MPI_Ssend(&v[y][Lx],  1, MPI_DOUBLE, decomp.east, 0, MPI_COMM_WORLD);
-//      MPI_Recv(&v[y][0],    1, MPI_DOUBLE, decomp.west, 0, MPI_COMM_WORLD, &status);
-// 
-//      MPI_Ssend(&v[y][1],   1, MPI_DOUBLE, decomp.west, 0, MPI_COMM_WORLD);
-//      MPI_Recv(&v[y][Lx+1], 1, MPI_DOUBLE, decomp.east, 0, MPI_COMM_WORLD, &status);
-//   }
-  
-//  TODO: geht noch nicht. "laplace: error: no convergence" mit der for-loop oben geht es wieder
+
   MPI_Datatype Column;
-  MPI_Type_vector(Ly, 1, Lx+1, MPI_DOUBLE, &Column);
+  MPI_Type_vector(Ly, 1, Lx+2, MPI_DOUBLE, &Column);
   MPI_Type_commit(&Column);
-  
+
   MPI_Sendrecv(&v[1][Lx], 1, Column, decomp.east, 0, &v[1][Lx+1], 1, Column, decomp.east, 0, MPI_COMM_WORLD, &status);
   MPI_Sendrecv(&v[1][1], 1, Column, decomp.west, 0, &v[1][0], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &status);
-  
+
   MPI_Type_free(&Column);
 }
