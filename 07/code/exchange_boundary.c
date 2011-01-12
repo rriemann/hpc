26c2e16c8fa27b44fd709351f577b4d2a0bb8356
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-12 14:12:07 +0100

    anfang aufg. 3

--- 07/code/exchange_boundary.c
+++ 07/code/exchange_boundary.c
@@ -7,24 +7,31 @@ extern Decomp decomp;
 void exchange_boundary(field v, int Lx, int Ly)
 {
   int y;
-  MPI_Status status;
+  MPI_Status status[4];
+  MPI_Request req[4];
 
   MPI_Datatype Row;
   MPI_Type_contiguous(Lx, MPI_DOUBLE, &Row);
   MPI_Type_commit(&Row);
 
-  MPI_Sendrecv(&v[Ly][1], 1, Row, decomp.north, 0, &v[Ly+1][1], 1, Row, decomp.north, 0, MPI_COMM_WORLD, &status);
+  MPI_Isend(&v[Ly][1],   1, Row, decomp.north, 0, MPI_COMM_WORLD, &req[0]);
+  MPI_Irecv(&v[Ly+1][1], 1, Row, decomp.north, 0, MPI_COMM_WORLD, &(*status));
 
-  MPI_Sendrecv(&v[1][1], 1, Row, decomp.south, 0, &v[0][1], 1, Row, decomp.south, 0, MPI_COMM_WORLD, &status);
-
-  MPI_Type_free(&Row);
+  MPI_Isend(&v[1][1], 1, Row, decomp.south, 0, MPI_COMM_WORLD, &req[1]);
+  MPI_Irecv(&v[0][1], 1, Row, decomp.south, 0, MPI_COMM_WORLD, &status[1]);
 
   MPI_Datatype Column;
   MPI_Type_vector(Ly, 1, Lx+2, MPI_DOUBLE, &Column);
   MPI_Type_commit(&Column);
 
-  MPI_Sendrecv(&v[1][Lx], 1, Column, decomp.east, 0, &v[1][Lx+1], 1, Column, decomp.east, 0, MPI_COMM_WORLD, &status);
-  MPI_Sendrecv(&v[1][1], 1, Column, decomp.west, 0, &v[1][0], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &status);
+  MPI_Isend(&v[1][Lx],   1, Column, decomp.east, 0, MPI_COMM_WORLD, &req[2]);
+  MPI_Irecv(&v[1][Lx+1], 1, Column, decomp.east, 0, MPI_COMM_WORLD, &status[2]);
 
+  MPI_Isend(&v[1][1], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &req[3]);
+  MPI_Irecv(&v[1][0], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &status[3]);
+
+  MPI_Waitall(4,req,status);
+
+  MPI_Type_free(&Row);
   MPI_Type_free(&Column);
 }
