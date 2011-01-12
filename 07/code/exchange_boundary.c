dbefb40ef74adbfeaf3dcbd772f23ba7e8c6919e
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-12 14:54:45 +0100

    aufgabe 4 geht soweit
    update gitignore

--- 07/code/exchange_boundary.c
+++ 07/code/exchange_boundary.c
@@ -20,11 +20,11 @@ void exchange_boundary(field v, int Lx, int Ly)
   MPI_Type_free(&Row);
 
   MPI_Datatype Column;
-  MPI_Type_vector(Ly, 1, Lx+2, MPI_DOUBLE, &Column);
+  MPI_Type_vector(Ly+2, 1, Lx+2, MPI_DOUBLE, &Column);
   MPI_Type_commit(&Column);
 
-  MPI_Sendrecv(&v[1][Lx], 1, Column, decomp.east, 0, &v[1][Lx+1], 1, Column, decomp.east, 0, MPI_COMM_WORLD, &status);
-  MPI_Sendrecv(&v[1][1], 1, Column, decomp.west, 0, &v[1][0], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &status);
+  MPI_Sendrecv(&v[0][Lx], 1, Column, decomp.east, 0, &v[0][Lx+1], 1, Column, decomp.east, 0, MPI_COMM_WORLD, &status);
+  MPI_Sendrecv(&v[0][1], 1, Column, decomp.west, 0, &v[0][0], 1, Column, decomp.west, 0, MPI_COMM_WORLD, &status);
 
   MPI_Type_free(&Column);
 }
