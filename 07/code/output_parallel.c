2f8473ef5f00c31f3533bcb74bd6a259e1b765c4
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-20 01:29:00 +0100

    aufgabe 6 läuft mit row

--- 07/code/output_parallel.c
+++ 07/code/output_parallel.c
@@ -1,6 +1,7 @@
 # include "mpi.h"
 # include "laplace.h"
 # include "decomp.h"
+# include <string.h>
 
 extern Decomp decomp;
 
@@ -10,26 +11,52 @@ void output_parallel(field v, int Lx, int Ly)
   int        x, y, x_local, y_local, home_of_xy;
   MPI_Status status;
 
+  MPI_Datatype Row;
+  MPI_Type_contiguous(decomp.Lx, MPI_DOUBLE, &Row);
+  MPI_Type_commit(&Row);
+//   MPI_Datatype Column;
+//   MPI_Type_vector(Ly, 1, Lx+2, MPI_DOUBLE, &Column);
+//   MPI_Type_commit(&Column);
+
   for (y = 0; y <= decomp.Ny + 1; y++) {
     for (x = 0; x <= decomp.Nx + 1; x++) {
       global2local(x, y, &x_local, &y_local, &home_of_xy);
 
+      if (x_local == 1) {
+        if (decomp.my_rank == 0) {
+          if (home_of_xy == 0) {
+            memcpy(&vv[y][x],&v[y][x],decomp.Lx*sizeof(double));
+          } else {
+            MPI_Recv(&vv[y][x], 1, Row, home_of_xy, 0, MPI_COMM_WORLD, &status);
+          }
+        } else {
+          if (decomp.my_rank == home_of_xy) {
+            MPI_Ssend(&v[y_local][x_local], 1, Row, 0, 0, MPI_COMM_WORLD);
+          }
+        }
+        x = x + decomp.Lx - 1;
+        continue;
+      }
+
       if (decomp.my_rank == 0) {
-	if (home_of_xy == 0) {
-	  vv[y][x] = v[y][x];
-	} else {
-	  MPI_Recv(&vv[y][x], 1, MPI_DOUBLE, home_of_xy, 0, MPI_COMM_WORLD, &status);
-	}
+        if (home_of_xy == 0) {
+          vv[y][x] = v[y][x];
+        } else {
+          MPI_Recv(&vv[y][x], 1, MPI_DOUBLE, home_of_xy, 0, MPI_COMM_WORLD, &status);
+        }
       } else {
-	if (decomp.my_rank == home_of_xy) {
-	  MPI_Ssend(&v[y_local][x_local], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-	}
+        if (decomp.my_rank == home_of_xy) {
+          MPI_Ssend(&v[y_local][x_local], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
       }
     }
   }
   
   if (decomp.my_rank == 0)
     output(vv, decomp.Nx, decomp.Ny);
+
+  MPI_Type_free(&Row);
+//   MPI_Type_free(&Column);
 
   field_free(vv);
 }
