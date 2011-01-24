ba3afb125cbd4c89ef9d4549e108dbc0fd351be2
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-01-24 02:57:38 +0100

    siebentens geht mit 2 2 und 5 4

--- 07/code/output_parallel.c
+++ 07/code/output_parallel.c
@@ -3,62 +3,62 @@
 # include "laplace.h"
 # include "decomp.h"
 # include <string.h>
+# include <stdio.h>
 
 extern Decomp decomp;
 
 void output_parallel(field v, int Lx, int Ly)
 {
-  field      vv = field_alloc(decomp.Ny, decomp.Nx);
-  int        x, y, i, x_local, y_local, x_global, y_global, home_of_xy;
-  MPI_Status status;
+  int x, y, i, x_local, y_local, x_global, y_global, home_of_xy;
+  field vv = field_alloc(decomp.Ny, decomp.Nx);
 
-  MPI_Datatype Row;
-  MPI_Type_contiguous(decomp.Lx, MPI_DOUBLE, &Row);
-  MPI_Type_commit(&Row);
+  // Rand durch erneuten einlesen setzen
+  FILE *data = fopen("input.data", "r");
+  double value;
 
+  while (fscanf(data, "%d %d %lg", &x, &y, &value) != EOF) {
+    vv[y][x] = value;
+  }
+  fclose(data);
+
+  // Anlegen des Datentyps zur Übertragung
   MPI_Datatype Block;
-//   if (decomp.my_rank == 0) {
-//     MPI_Type_vector(decomp.Ly, 1, decomp.Nx+2, Row, &Block);
-//   } else {
-    MPI_Type_vector(decomp.Ly, 1, decomp.Lx+2, Row, &Block);
-//   }
+
+  if (decomp.my_rank == 0) {
+    MPI_Type_vector(decomp.Ly, decomp.Lx, decomp.Nx+2, MPI_DOUBLE, &Block);
+  } else {
+    MPI_Type_vector(decomp.Ly, decomp.Lx, decomp.Lx+2, MPI_DOUBLE, &Block);
+  }
   MPI_Type_commit(&Block);
 
+  // Einsammeln der Daten und abspeichern bei rank 0
+  MPI_Status status;
 
-  if (decomp.my_rank == 0) {
-    for (y = 0; y < decomp.procs_y; ++y) {
-      for (x = 0; x < decomp.procs_x; ++x) {
-        x_global = x*decomp.Lx+1;
-        y_global = y*decomp.Ly+1;
-        global2local(x_global, y_global, &x_local, &y_local, &home_of_xy);
-        printf("%d %d %d\n",x,y,home_of_xy);
+  for (y = 0; y < decomp.procs_y; ++y) {
+    for (x = 0; x < decomp.procs_x; ++x) {
+      x_global = x*decomp.Lx+1;
+      y_global = y*decomp.Ly+1;
+      global2local(x_global, y_global, &x_local, &y_local, &home_of_xy);
+      if (decomp.my_rank == 0) {
         if (home_of_xy == 0) {
-          for (i = 0; i <= decomp.Ly; ++i) {
-            memcpy(&vv[y_global+i][x_global+1],&v[1+i][1],decomp.Lx*sizeof(double));
+          for (i = 0; i < decomp.Ly; ++i) {
+            memcpy(&vv[y_global+i][x_global],&v[1+i][1],decomp.Lx*sizeof(double));
           }
         } else {
-//           MPI_Recv(&vv[y_global][x_global], 1, Block, home_of_xy, 0, MPI_COMM_WORLD, &status);
+            MPI_Recv(&vv[y_global][x_global], 1, Block, home_of_xy, 0, MPI_COMM_WORLD, &status);
         }
-        printf("x\n\n",x,y);
+      } else {
+        if (home_of_xy == decomp.my_rank) {
+          MPI_Ssend(&v[1][1], 1, Block, 0, 0, MPI_COMM_WORLD);
+        }
       }
     }
-  } else {
-//     MPI_Ssend(&v[1][1], 1, Block, 0, 0, MPI_COMM_WORLD);
   }
 
-  // TODO Randaustausch
-
   if (decomp.my_rank == 0) {
-    memcpy(&vv[0,0],&v[0,0],(decomp.Nx+2)*sizeof(double)); // bottom
-    memcpy(&vv[decomp.Ny+1,0],&v[decomp.Ly+1,0],(decomp.Nx+2)*sizeof(double)); // top
-    for (i = 1; i < decomp.Ny + 1; ++i) {
-      vv[i,0] = v[i,0]; // left
-      vv[i,decomp.Nx+1] = v[i,decomp.Nx+1]; // right
-    }
     output(vv, decomp.Nx, decomp.Ny);
   }
 
-  MPI_Type_free(&Row);
   MPI_Type_free(&Block);
 
   field_free(vv);
