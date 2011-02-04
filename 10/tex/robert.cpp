c4dada10ebe220a741b3d72b18ae456f32fa085a
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-02-04 21:31:55 +0100

    mein vorschlag

---
+++ 10/tex/robert.cpp
@@ -0,0 +1,25 @@
+int length = sizeof(a)/sizeof(a[0]);
+int number_of_threads = omp_get_num_threads();
+int piece_length = length/number_of_threads;
+#pragma omp parallel
+{
+  for (int i = 1+omp_get_thread_num()*piece_length; i <= (omp_get_thread_num()+1)*piece_length; ++i) {
+    a[i] += a[i-1];
+  }
+}
+for (int i = 1; i < number_of_threads; ++i) { // determine constants
+  a[2*piece_length-1] += a[piece_lenght-1];
+}
+#pragma omp parallel
+{
+  if (omp_get_thread_num() == 0) { // wenn length/number_of_threads einen rest hat, berechne fehlende elemente
+    for (int i = number_of_threads*piece_length+1; i < length; ++i) {
+      a[i] += a[i-1];
+    }
+  } else { // apply constants to all other elements
+    int copy_from_index = omp_get_thread_num()*piece_length-1;
+    for (int i = copy_from_index+1; i < (omp_get_thread_num()+1)*piece_length-1; ++i) {
+      a[i] += a[copy_from_index];
+    }
+  }
+}
