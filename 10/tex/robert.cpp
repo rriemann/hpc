5d8c324ddce42b2c7adbd96467b25706cf6a5fc2
Thomas Murach <asiasuppenesser@gmx.de>
2011-02-07 17:27:10 +0100

    tex fertig

--- 10/tex/robert.cpp
+++ 10/tex/robert.cpp
@@ -1,6 +1,6 @@
-int length = sizeof(a)/sizeof(a[0]);
+int length = sizeof(a)/sizeof(a[0]);           // Zahl der Elemente des Arrays a
 int number_of_threads = omp_get_num_threads();
-int piece_length = length/number_of_threads;
+int piece_length = length/number_of_threads;   // Division mit Rest
 #pragma omp parallel
 {
   for (int i = 1+omp_get_thread_num()*piece_length; i <= (omp_get_thread_num()+1)*piece_length; ++i) {
@@ -13,7 +13,7 @@ for (int i = 1; i < number_of_threads; ++i) { // determine constants
 #pragma omp parallel
 {
   if (omp_get_thread_num() == 0) { // wenn length/number_of_threads einen rest hat, berechne fehlende elemente
-    for (int i = number_of_threads*piece_length+1; i < length; ++i) {
+    for (int i = 1+number_of_threads*piece_length; i < length; ++i) {
       a[i] += a[i-1];
     }
   } else { // apply constants to all other elements
