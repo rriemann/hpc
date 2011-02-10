ced0218d0bc0f8782b0df58faeede8c758ab83f4
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2011-02-10 01:06:46 +0100

    Kommentiere, warum output.c nicht parallelisiert werden kann

--- 11/code/output.c
+++ 11/code/output.c
@@ -5,6 +5,9 @@ void output(field v, int Nx, int Ny)
 {
     int x, y;
 
+    // Ausgaben zu parallisieren macht keinen Sinn. Zeitintensiv ist hier das
+    // printf, welches an sich nicht multithreadingfähig ist, da es bei IO
+    // sonst zu Race-Conditions kommen könnte.
     for (y = Ny + 1; y >= 0; y--) {
         printf("%4i     ", y);
         for (x = 0; x <= Nx + 1; x++)
@@ -14,6 +17,7 @@ void output(field v, int Nx, int Ny)
 
     printf("   ^     \n");
     printf("   y  x >");
+    // gleiches Argument wie oben
     for (x = 0; x <= Nx + 1; x++)
         printf("%6i   ", x);
     printf("\n");
